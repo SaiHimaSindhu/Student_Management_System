eb7dd395e655e63c884701eeb37851a23ec02467
SaiHimaSindhu <sindhukancherla2003@gmail.com>
2026-09-21 15:00:32 +0530

    Switch to SQLite

--- student_management/settings.py
+++ student_management/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'student_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'student_management_system',
-        'USER': 'root',
-        'PASSWORD': 'kancherla8123',
-        'HOST': 'localhost',
-        'PORT': '3306',
+       'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
