fc7a2ca8c438215bc82003475b6e293b2f7253d5
SaiHimaSindhu <sindhukancherla2003@gmail.com>
2026-09-21 11:57:58 +0530

    Modified data

--- student_management/settings.py
+++ student_management/settings.py
@@ -31,16 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'stuents.apps.StudentsConfig',
+    'students.apps.StudentsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-
-    # Local apps
-    'students',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
